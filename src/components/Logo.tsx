2bb4b6334e59ee0a1fb27ee1cbec3c38ca1e0792
Qir05 <ebberdos@gmail.com>
2026-06-15 10:43:26 -0700

    Use Phases brand logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,7 +8,7 @@ export default function Logo({ className = '', size = 40 }: LogoProps) {
     <div className={`flex items-center gap-2 ${className}`}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/phases-logo.svg"
+        src="/phases-logo.png"
         alt="Phases"
         width={size}
         height={size}
